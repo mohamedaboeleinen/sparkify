07248102725e64b4c6287d920e53c08c70abb6bc
mohamedaboeleinen <mohammad.aboeleinen@gmail.com>
2022-11-30 15:23:12 +0200

    adding CREATE and DROP table statements

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,63 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE songplays"
+user_table_drop = "DROP TABLE users"
+song_table_drop = "DROP TABLE songs"
+artist_table_drop = "DROP TABLE artists"
+time_table_drop = "DROP TABLE time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays ( 
+                            songplay_id int PRIMARY KEY, 
+                            start_time timestamp,
+                            user_id int, 
+                            level varchar,
+                            song_id int, 
+                            session_id int, 
+                            location varchar, 
+                            user_agent varchar
+
+);
 """)
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (
+                        user_id int PRIMARY KEY, 
+                        first_name varchar, 
+                        last_name varchar, 
+                        gender varchar, 
+                        level varchar
+
+);
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (
+                        song_id int PRIMARY_KEY,
+                        title varchar, 
+                        artist_id int, 
+                        year int, 
+                        duration int
+);
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (
+                        artist_id int PRIMARY_KEY,
+                        name varchar, 
+                        location varchar, 
+                        latitude int, 
+                        longitude int
+);
 """)
 
-time_table_create = ("""
+time_table_create = (""" CREATE TABLE IF NOT EXISTS time (
+                         start_time timestamp, 
+                         hour int, 
+                         day varchar, 
+                         week int, 
+                         month varchar, 
+                         year int, 
+                         weekday varchar
+);
 """)
 
 # INSERT RECORDS
